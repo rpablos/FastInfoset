d7d0a8ebf23a637311a9fd8ed67b1353c29f7f94
Ronald <Ronald@athlon>
2014-05-09 15:10:28 +0200

    comment headers

--- FastInfoset/src/fastinfoset/Canonical_DOM_FI_Encoder.java
+++ FastInfoset/src/fastinfoset/Canonical_DOM_FI_Encoder.java
@@ -1,8 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//  Author: Ronald Pablos
+//  Year: 2014
 
 package fastinfoset;
 
