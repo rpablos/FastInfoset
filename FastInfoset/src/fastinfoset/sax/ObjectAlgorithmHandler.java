c9c774dd7381ba6c5c8939af43f0ef470db7aafc
Ronald Pablos <>
2014-06-19 08:10:08 +0200

    Apache 2.0 license

--- FastInfoset/src/fastinfoset/sax/ObjectAlgorithmHandler.java
+++ FastInfoset/src/fastinfoset/sax/ObjectAlgorithmHandler.java
@@ -1,5 +1,18 @@
-//  Author: Ronald Pablos
-//  Year: 2013
+/*
+* Copyright 2014 Ronald Pablos.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 package fastinfoset.sax;
 
@@ -11,12 +24,20 @@ import org.xml.sax.SAXException;
  * @author rpablos
  */
 public interface ObjectAlgorithmHandler {
+
+    /**
+     * Receive notification of algorithm encoded object
+     * 
+     * @param object the object
+     * @param algorithm the algorithm
+     * @throws SAXException
+     */
     public void object(Object object, Algorithm algorithm) throws SAXException;
     /**
      * Receive notification of algorithm encoded object
      * 
      * Only called when the algorithm is not available or there is no URI
-     * Otherwise, the object method is called
+     * Otherwise, other <code>object</code> method is called
      * @param data encoded data
      * @param algorithmId index for the algorithm
      * @throws org.xml.sax.SAXException
